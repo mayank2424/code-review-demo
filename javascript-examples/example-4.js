a4029aa67fb71a6f2fea3c79ccf57991f8756425
Mayank Gupta <mayank.gupta@crowdlinker.com>
2023-08-07 15:00:55 +0530

    Added missing request body

--- javascript-examples/example-4.js
+++ javascript-examples/example-4.js
@@ -16,3 +16,51 @@ app.post('/login', (req, res) => {
       }
     });
   });
+
+
+  app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+      
+      const isPasswordValid = await user.comparePassword(password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+      
+      res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+      console.error('Login error:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+
+class AuthController {
+    constructor() {}
+
+    async login(req, res) {
+        try {
+            const { username, password } = req.body;
+            const user = await User.findOne({ username });
+            if (!user || !this.isPasswordValid(password, user.password)) {
+              return res.status(401).json({ error: 'User not found or Invalid credentials!' });
+            }
+
+            res.status(200).json({ message: 'Login successful' });
+
+        } catch(error) {
+            console.error('Login error:', error);
+            res.status(500).json({ error: 'Server error' });
+        }
+    }
+
+    isPasswordValid(inputPassword, userPassword) {
+        return bcrypt.compare(inputPassword, userPassword);
+    }
+        
+}
+
+app.post('/login', AuthController.login);
